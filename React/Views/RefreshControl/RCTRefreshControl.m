6d02d0e25fc5aeb156b7f12c73a7010b242915a2
Taylor123 <cjohns28@terpmail.umd.edu>
2020-10-06 18:22:13 -0700

    add iOS support for progressViewOffset on RefreshControl

--- React/Views/RefreshControl/RCTRefreshControl.m
+++ React/Views/RefreshControl/RCTRefreshControl.m
@@ -21,6 +21,7 @@
   BOOL _refreshingProgrammatically;
   NSString *_title;
   UIColor *_titleColor;
+  float _progressViewOffset;
 }
 
 - (instancetype)init
@@ -53,6 +54,8 @@ RCT_NOT_IMPLEMENTED(-(instancetype)initWithCoder : (NSCoder *)aDecoder)
     [self beginRefreshingProgrammatically];
   }
   _isInitialRender = false;
+
+  self.frame = CGRectOffset(self.frame, 0, _progressViewOffset);
 }
 
 - (void)beginRefreshingProgrammatically
@@ -156,6 +159,11 @@ RCT_NOT_IMPLEMENTED(-(instancetype)initWithCoder : (NSCoder *)aDecoder)
 {
   _currentRefreshingState = refreshing;
   _currentRefreshingStateTimestamp = _currentRefreshingStateClock++;
+}
+
+- (void)setProgressViewOffset:(float)offset
+{
+	_progressViewOffset = offset / UIScreen.mainScreen.scale;
 }
 
 - (void)refreshControlValueChanged
